7e77401805b9a5630bbf39afe2d04a247ad8857e
xiaojun wang <xwang48@tulane.edu>
2025-04-07 14:53:53 -0500

    update basic vue.config

--- vue.config.js
+++ vue.config.js
@@ -24,7 +24,7 @@ module.exports = {
    * In most cases please use '/' !!!
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
-  publicPath: '/vue-test/',
+  publicPath: '/TUMMT/',
   outputDir: 'dist',
   assetsDir: 'static',
   lintOnSave: process.env.NODE_ENV === 'development',
@@ -38,7 +38,7 @@ module.exports = {
     },
     proxy: {
       '/': {
-        target: 'http://localhost:8000/',
+        target: 'http://localhost:6000/',
         changeOrigin: true,
         pathRewrite: {
           '^/': ''
